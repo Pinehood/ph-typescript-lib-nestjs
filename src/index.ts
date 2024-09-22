d53d934df45d6390d1612e7d30bb09d524b8a808
fiki574 <fiki8796@hotmail.com>
2024-09-22 15:11:11 +0200

    chore(deps): Sentry removal

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ export * from "./comms";
 export * from "./maps";
 export * from "./oauth";
 export * from "./redis";
-export * from "./sentry";
 export * from "./stripe";
 export * from "./typeorm";
 export * from "./utils";
