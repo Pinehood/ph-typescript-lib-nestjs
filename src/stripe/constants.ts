1b1cdb5938b44bd4bdef9b9f5a3374b77f222080
fiki574 <fiki8796@hotmail.com>
2024-11-10 18:04:05 +0100

    chore(deps): package version upgrades

--- src/stripe/constants.ts
+++ src/stripe/constants.ts
@@ -1,7 +1,7 @@
 export enum StripeConstants {
   TEST_MODE = "test",
   LIVE_MODE = "live",
-  API_VERSION = "2024-06-20",
+  API_VERSION = "2024-10-28.acacia",
   DASHBOARD_SANDBOX = "https://dashboard.stripe.com/test/payments/",
   DASHBOARD_PRODUCTION = "https://dashboard.stripe.com/live/payments/",
   HEADER_SIGNATURE = "stripe-signature",
